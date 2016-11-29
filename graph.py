a82d19a21687c82e5d3c57b9be3aa6d9ad4b8235
Vikas Palakurthi <vikaspro90@gmail.com>
2016-11-28 21:22:15 -0500

    test

--- graph.py
+++ graph.py
@@ -2,31 +2,51 @@ class Vertex():
 	def __init__(self, key):
 		self.id = key
 		self.connectedTo = []
-	def addNeighbor(self, key):
+	def addNeighbor(self,key):
 		self.connectedTo.append(key)
 	def __str__(self):
-		return str(self.id) + "connected to " + (", ".join([x for x in self.connectedTo]) or "None")
+		return str(self.id) + "connected to " + ", ".join([x for x in self.connectedTo])
+	def getConnections(self):
+		return self.connectedTo
+	def getId(self):
+		return self.id
+
 class Graph:
 	def __init__(self):
 		# create a new graph
-		self.vertices = []
+		self.vertices = {}
 		self.numOfVertices = 0
 
 	def addVertex(self, vertex):
 		# add a new vertex to the graph
-		pass
+		newVertex = Vertex(vertex)
+		self.vertices[vertex] = newVertex
+		self.numOfVertices += 1
+		return newVertex
+
 	def addEdge(self, fromVertex, toVertex):
-		# add an edge
-		pass
-	def addEdge(self, fromVertex, toVertex, weight):
 		# add a weighted edge
-		pass
+		if fromVertex not in self.vertices:
+			nv = self.addVertex(fromVertex)
+		if toVertex not in self.vertices:
+			nv = self.addVertex(toVertex)
+		self.vertices[fromVertex].addNeighbor(self.vertices[toVertex])
+
 	def getVertex(self, vertKey):
-		# get teh vertex with the name vertexKey
-		pass
+		# get the vertex with the name vertexKey
+		if vertKey in self.vertices:
+			return self.vertices[vertKey]
+		else:
+			return None
+
 	def getVertices(self):
 		# get all vertices
-		pass
+		return self.vertices.keys()
+
 	def __contains__(self, vertex):
 		# return true if vertex is in graph, else false
-		pass
+		return vertex in self.vertices.keys()
+
+	def __iter__(self):
+		# Iterate over all vertices objects in graph
+		return iter(self.vertices.values())
